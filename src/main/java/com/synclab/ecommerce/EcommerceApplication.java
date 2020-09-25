c7234c24791808691ecd8eb4f7bcde73851256ef
unknown <stoughtonrich@gmail.com>
2020-09-25 22:01:40 +0200

    quello dopo

--- src/main/java/com/synclab/ecommerce/EcommerceApplication.java
+++ src/main/java/com/synclab/ecommerce/EcommerceApplication.java
@@ -8,6 +8,13 @@ public class EcommerceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EcommerceApplication.class, args);
+		clearScreen();
+	}
+
+	public static void clearScreen() {
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+		System.out.println("Spring server started (✓) - Waiting for commands...");
 	}
 
 }
